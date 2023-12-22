5b5ce0f99d07e981181b363d099e5bdec6678b42
siegward-from <86006595+siegward-from@users.noreply.github.com>
2023-12-22 11:34:08 +0100

    creation api

--- back/edf/urls.py
+++ back/edf/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('Typologie/', views.getAllTypologie, name='Typologie'),
+    path('Ecogestes/', views.getAllEcogestes, name='Ecogestes'),
+    path('Users/', views.getAllUsers, name='Users'),
+    path('new-user/', views.add_new_user, name='new-user'),
 ]
--- back/edf/views.py
+++ back/edf/views.py
@@ -13,9 +13,7 @@ def index(request):
 
 @api_view(['GET'])
 def getAllTypologie(request):
-
     typologies = Typologie.objects.all()
-    # serialize project objects returned in the queryset
     serializer = TypologieSerializer(typologies, many=True)
     if serializer.data is not None:
         if len(serializer.data) > 0:
@@ -24,3 +22,46 @@ def getAllTypologie(request):
             return Response({'message': 'no projects found, empty querySet', 'success': True}, status=200)
     else:
         return Response({'message': 'something went wrong !', 'success': False}, 400)
+
+
+@api_view(['GET'])
+def getAllEcogestes(request):
+    ecogestes = Ecogestes.objects.all()
+    serializer = EcogestesSerializer(ecogestes, many=True)
+    if serializer.data is not None:
+        if len(serializer.data) > 0:
+            return Response(serializer.data, status=200)
+        else:
+            return Response({'message': 'no projects found, empty querySet', 'success': True}, status=200)
+    else:
+        return Response({'message': 'something went wrong !', 'success': False}, 400)
+
+
+@api_view(['GET'])
+def getAllUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    if serializer.data is not None:
+        if len(serializer.data) > 0:
+            return Response(serializer.data, status=200)
+        else:
+            return Response({'message': 'no projects found, empty querySet', 'success': True}, status=200)
+    else:
+        return Response({'message': 'something went wrong !', 'success': False}, 400)
+
+
+@api_view(['POST'])
+def add_new_user(request):
+    data = request.data
+    serializer = UserSerializer(data=data)
+    if serializer.is_valid():
+        new_user = serializer.save()
+        apiResponse = {'message': 'project ' + new_user.title + ' created successefully', 'success': True}
+        return Response(apiResponse, status=201)
+    else:
+        errors = serializer.errors
+        errors_to_render = []
+        for key in errors.keys():
+            errors_to_render.append({key: errors[key]})
+        apiResponse = {'message': 'form errors', 'success': True, 'errors': errors_to_render}
+        return Response(apiResponse)
